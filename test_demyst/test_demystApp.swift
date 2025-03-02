4486de44c46fb11e016a93b8f885e7eeecf16cf8
Muhammad Saud Anwar <saudanwar3@hotmail.com>
2025-03-02 21:04:35 +0400

    Initial Xcode Project Commit

---
+++ test_demyst/test_demystApp.swift
@@ -0,0 +1,17 @@
+//
+//  test_demystApp.swift
+//  test_demyst
+//
+//  Created by Muhammad Saud Anwar on 02/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct test_demystApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ test_demystTests/test_demystTests.swift
@@ -0,0 +1,17 @@
+//
+//  test_demystTests.swift
+//  test_demystTests
+//
+//  Created by Muhammad Saud Anwar on 02/03/2025.
+//
+
+import Testing
+@testable import test_demyst
+
+struct test_demystTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
